9e17b4ad7832f13ab7c39a66ec93534671ba034c
cthedj <114969577+cthedj@users.noreply.github.com>
2026-08-25 12:35:55 +0200

    Wire production authentication bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MsalProvider } from '@azure/msal-react'
 import App from './App'
+import AuthGate from './auth/AuthGate'
+import { msalInstance } from './auth/msal'
+import { runtimeConfig } from './config/runtime'
 import './styles.css'
+
+const app = (
+  <AuthGate>
+    <App />
+  </AuthGate>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {runtimeConfig.authMode === 'entra'
+      ? <MsalProvider instance={msalInstance}>{app}</MsalProvider>
+      : app}
   </React.StrictMode>,
 )
